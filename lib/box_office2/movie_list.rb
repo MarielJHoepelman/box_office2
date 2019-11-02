e35f930a4a51c3713d8f4b7a744fc571c3e72e86
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-11-02 18:46:33 -0400

    Refactors movie_list file to scrape box office date.

--- lib/box_office2/movie_list.rb
+++ lib/box_office2/movie_list.rb
@@ -1,13 +1,21 @@
 module BoxOffice2
   class MovieList
-    attr_reader :list
+    attr_reader :list, :weekend_date
 
     def initialize
-      @list = scrape_imdb
+      @list = scrape_list
+      @weekend_date = scrape_weekend_date
     end
 
-    def scrape_imdb
-      doc = BoxOffice2::Scrapper.scrapper("chart/boxoffice")
+    def doc
+      @doc ||= BoxOffice2::Scrapper.scrapper("chart/boxoffice")
+    end
+
+    def scrape_weekend_date
+      doc.css("#boxoffice h4").text
+    end
+
+    def scrape_list
       movies_list = doc.css(".chart .titleColumn")
 
       movies_list.map do |movie|
